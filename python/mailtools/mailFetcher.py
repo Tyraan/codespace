f84c69316b826ff7bfac185c177d0a5aa4d5faac
Tyraan <tomkly000@gmail.com>
2014-09-16 18:55:00 +0800

    finished mailtools's fetchmaill class and it's sublclass Date 2014/9/16

--- python/mailtools/mailFetcher.py
+++ python/mailtools/mailFetcher.py
@@ -23,8 +23,11 @@ class MailFetcher(MailTool):
 
     def connect(self):
         self.trace('Connecting ...')
+
         self.getPassword()
+
         server = poplib.POP3(self.popServer)
+
         server.user(self.popUser)
         server.pass_(self.popPassword)
         self.trace(server.getwelcome())
@@ -101,17 +104,144 @@ class MailFetcher(MailTool):
             return allhdrs,allsizes,False
 
     def downloadAllMessages(self,progress=None,loadfrom=1):
+        self.trace('downloading full messages ')
+        fetchlimit = pp13.mailconfig.fetchlimit
+        server = self.connect()
+        try:
+            (msgCount,msgBytes) = server.stat()
+            allmsgs  = []
+            allsizes = []
+            for i in range(loadfrom, msgCount+1):
+                if progress:progress(i,msgCount)
+                if fetchlimit and (i<=msgCount - fetchlimit):
+                    mailtext = 'Subject :--mail skiped -- \n\nMail skipped.\n'
+                    allmsgs.append(mailtext)
+                    allsizes.append(len(mailtext))
+                else:
+                    (resp,message,respsz) = server.retr(i)
+                    message = self.decodeFullText(message)
+                    allmsgs.append('\n'.join(message))
+                    allsizes.append(respsz)
+        finally:
+            server.quit()
+        assert len(allmsgs)==(msgCount-loadfrom)+1
+        return allmsgs,allsizes,True
+
+
+    def deleteMessages(self,msgnums,progress=None):
+        self.trace('deleting mails...')
+        server = self.connect()
+        try:
+            for (ix, msgnum) in enumerate(msgnums):
+                if progress:progress(ix+1,len(msgnums))
+                server.dele(msgnum)
+        finally:
+            server.quit()
 
 
 
-    def deleteMessages(self):
 
-    def deletemessageSafely(self):
 
-    def checkSynchError(self):
 
-    def headerMatch(self):
+
+
+
+    def deletemessageSafely(self,msgnums,synchHeaders,progress=None):
+        if not self.srvrHasTop:
+            raise TopNotSupported('Safe delete cancelled.')
+        self.trace('deleting mails safely')
+        errmsg = 'Message %s out of synch with server.\n'
+        errmsg +='Delete terminated at this message.\n'
+        errmsg += 'Mail client may require restart or reload.'
+
+        server = self.connect()
+        try:
+            (msgCount,msgBytes) = server.stat()
+            for (ix,msgnum)in enumerate(msgnums):
+                if progress:progress(ix+1,len(msgnums))
+                if msgnum > msgCount:
+                    raise DeleteSynchError(errmsg% msgnum)
+                resp,hdrlines,respsz = server.top(msgnum, 0)
+                hdrlines = self.decodeFullText(hdrlines)
+                msghdrs = '\n'.join(hdrlines)
+
+                if not self.headerMatch(msghdrs,synchHeaders[msgnum-1]):
+                    raise DeleteSynchError(errmsg % msgnum)
+                else:
+                    server.dele(msgnum)
+        finally:
+            server.quit()
+
+
+
+
+    def checkSynchError(self,synchHeaders):
+        self.trace('synch check')
+        errormsg = 'Message index out of synch with mail server.\n'
+        errormsg +='Mail clien may require restart or reload.'
+        sever = self.connect()
+        try:
+            lastmsgnum = len(synchHeaders)
+            (msgCount,msgBytes) = server.stat()
+            if lastmsgnum>msgCount:
+                raise MessageSynchError(errormsg)
+            if self.srvrHasTop:
+                resp,hdrlines,respz = server.top(lastmsgnum,0)
+                hdrlines = self.decodeFullText(hdrlines)
+                lastmsghdrs = '\n'.join(hdrlines)
+                if not self.headerMatch(lastmsghdrs,synchHeaders[-1]):
+                    raise MessageSynchError(errormsg)
+        finally:
+            server.quit()
+
+
+
+    def headerMatch(self,hdrtext1,hdrtext2):
+        if hdrtext1==hdrtext2:
+            return True
+        split1 = hdrtext1.splitlines()
+        split2 = hdrtext2.splitlines()
+        strip1 = [line for line in split1 if not line.startswith('Status:')]
+        strip2 = [line for line in split2 if not line.startswith('Status:')]
+        if strip1==strip2:
+            self.trace('same without status.')
+            return True
+        msgid1 = [line for line in split1 if line[:11].lower()=='message-id:']
+        msgid2 = [line for line in split2 if line[:11].lower()=='message-id:']
+        if (msgid1 or msgid2) and (msgid1 != msgid2):
+            self.trace('different message id ')
+
+            return False
+        tryheaders = ('From','To','Subject','Date')
+        tryheaders+=( 'Cc','Return-Path','Received')
+        msg1 = MailParser().parserHeaders(hdrtext1)
+        msg2 = MailParser().parserHeaders(hdrtext2)
+        for hdr in tryheaders:
+            if msg1.get_all(hdr) != msg2.get_all(hdr):
+                self.trace('Different common headers ')
+                return False
+        self.trace('Same common headers')
+        return True
+
+
     def getPassword(self):
+
+        if not self.popPassword:
+            try:
+                localfile = open('pp13.mailconfig.poppasswdfile')
+                self.popPassword = localfile.readline()[:-1]
+                self.trace('localfile password'+ repr(self.popPassword))
+
+            except:
+                self.popPassword = self.askPopPassword()
+
+    def askPopPassword(self):
+        assert False,'subclass must  define method for askPopPassword'
+
+
+
+
+
 
 
 
@@ -120,6 +250,10 @@ class MailFetcher(MailTool):
 ############################################################################################################
 class MailFetcherConsole(MailFetcher):
     def askPopPassword(self):
+        import getpass
+        promt = 'Password for %s on %s'%(self.popUser,self.popServer)
+        return getpass.getpass(promt)
+
 
 
 class SilentMailFetcher(SilentMailTool,MailFetcher):
